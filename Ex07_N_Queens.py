3b17bc3239c7dfe576dbcd66ae8de70e8b8e6331
Mohamed Rashid <moh.rashid20006@gmail.com>
2025-08-04 07:21:41 +0530

    Update Ex07_N_Queens.py

--- Ex07_N_Queens.py
+++ Ex07_N_Queens.py
@@ -1,1 +1,39 @@
+# Ex No: 7 - IMPLEMENTATION OF N-QUEEN’S PROBLEM USING BACKTRACKING METHOD
 
+def isSafe(mat, r, c):
+    for i in range(r):
+        if mat[i][c] == 'Q':
+            return False
+    i, j = r, c
+    while i >= 0 and j >= 0:
+        if mat[i][j] == 'Q':
+            return False
+        i -= 1
+        j -= 1
+    i, j = r, c
+    while i >= 0 and j < len(mat):
+        if mat[i][j] == 'Q':
+            return False
+        i -= 1
+        j += 1
+    return True
+
+def printSolution(mat):
+    for row in mat:
+        print(' '.join(row))
+    print()
+
+def nQueen(mat, r):
+    if r == len(mat):
+        printSolution(mat)
+        return
+    for i in range(len(mat)):
+        if isSafe(mat, r, i):
+            mat[r][i] = 'Q'
+            nQueen(mat, r + 1)
+            mat[r][i] = '-'
+
+N = int(input("Enter the number of Queens to be placed: "))
+mat = [['-' for _ in range(N)] for _ in range(N)]
+print("The possible solutions are:")
+nQueen(mat, 0)
